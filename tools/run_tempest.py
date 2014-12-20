bf7fa955f070e957082a2c4a8c2d88724825986c
Richard Winters <ric.winters@verizon.net>
2014-12-20 15:32:33 -0500

    Add testresourses pip install on remote this time

--- tools/run_tempest.py
+++ tools/run_tempest.py
@@ -38,6 +38,7 @@ def main(host, user, password, tempest_filter, tempest_dir, tempest_list_file,
             if tempest_list_file:
                 api.put(local_path=tempest_list_file, remote_path='list.txt')
             api.sudo(command='pip install junitxml')
+            api.sudo(command='pip install testresources')
             if patch_set:
                 api.run('git fetch https://review.openstack.org/openstack/tempest'
                         ' {0} && git checkout FETCH_HEAD'.format(patch_set))
